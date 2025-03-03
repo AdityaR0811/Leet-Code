09b8d63cb9c7f434bd3fa43ad903f1752a2a6767
AdityaR0811 <adityar.cse2026@gmail.comq>
2025-03-04 00:14:26 +0530

    Problem Difficulty Level : Medium

---
+++ Two Pointer/75. Sort Colors.cpp
@@ -0,0 +1,43 @@
+
+    /*
+    Time complexity : O(n)
+    Space complexity : O(1)
+    */
+    
+    
+    /*
+    Additional Notes : None
+    */
+
+    
+    class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = 0;
+        while(mid <= right)
+        {
+            if(nums[mid] == 0){
+                swap(nums[left], nums[mid]);
+                left++;
+                mid++;
+            }
+            else if(nums[mid] == 2){
+                swap(nums[right], nums[mid]);
+                right--;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+        }
+    }
+};
+    
+    
+    /*
+    Author : AdityaR0811
+    Question Link : https://leetcode.com/problems/sort-colors/description/
+    */
+    
+    
