dfee940c84804a01ed370ead3ad39ccb86c71cf6
AdityaR0811 <adityar.cse2026@gmail.comq>
2025-03-04 23:57:04 +0530

    Problem Difficulty Level : Medium

---
+++ Sorting/347. Top K Frequent Elements.cpp
@@ -0,0 +1,45 @@
+
+    /*
+    Time complexity : O(N log k)
+    Space complexity : O(N)
+    */
+    
+    
+    /*
+    Additional Notes : None
+    */
+
+    
+    class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> result;
+        for(auto& i: nums){
+            if(result.count(i)){
+                result[i]++;
+            }
+            else{
+                result[i] = 1;
+            }
+        }
+        vector<pair<int, int>> pairs;
+        for(auto itr: result)
+        {
+            pairs.push_back({itr.second , itr.first});
+        }
+        vector<int> answer;
+        sort(pairs.rbegin(), pairs.rend());
+        for(int i = 0 ; i < k ;i++){
+            answer.push_back(pairs[i].second);
+        }
+        return answer;
+    }
+};
+    
+    
+    /*
+    Author : AdityaR0811
+    Question Link : https://leetcode.com/problems/top-k-frequent-elements/
+    */
+    
+    
